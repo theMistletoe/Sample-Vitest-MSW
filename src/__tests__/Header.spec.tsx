548bef895ef4a9a63270cad9575f757a2133b12c
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-05 11:23:05 +0900

    テストライブラリのインポートを追加

--- src/__tests__/Header.spec.tsx
+++ src/__tests__/Header.spec.tsx
@@ -1,7 +1,7 @@
+import '@testing-library/jest-dom'
 import { describe, expect, it } from "vitest";
 import Header from "../Header";
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom'
 
 describe("Header", () => {
     it("見出しメッセージが見える", () => {
