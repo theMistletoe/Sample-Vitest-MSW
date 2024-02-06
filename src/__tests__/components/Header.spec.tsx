13d97d593fdb5230189b41b7255d8ae9871cac0c
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-06 10:53:40 +0900

    カウンターとヘッダーのテストを追加

--- src/__tests__/components/Header.spec.tsx
+++ src/__tests__/components/Header.spec.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import { describe, expect, it } from "vitest";
-import Header from "../Header";
+import Header from "../../components/Header";
 import { render, screen } from "@testing-library/react";
 
 describe("Header", () => {
