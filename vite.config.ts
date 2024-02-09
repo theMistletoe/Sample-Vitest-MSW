0a254a3f7fe35caa0ebf204b83a3e5587123566a
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-09 11:32:40 +0000

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "127.0.0.1",
+    open: true,
+  },
   test: {
     globals: true,
     environment: "jsdom",
