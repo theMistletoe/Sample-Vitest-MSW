efc6712d3c32620054171a24f87d9730d800d181
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-02-10 15:00:22 +0000

    sonarQubeの設定

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,5 +9,16 @@ export default defineConfig({
         globals: true,
         environment: "jsdom",
         setupFiles: "./setup.ts",
+        coverage: {
+            provider: 'v8',
+            include: ['src/**/*.{tsx,js,ts}'],
+            all: true,
+            // lcovを追加
+            reporter: ['html', 'clover', 'text', 'lcov']
+          },
+          root: '.',
+          // vitest-sonar-reporterを追加
+          reporters: ['verbose', 'vitest-sonar-reporter'],
+          outputFile: 'test-report.xml'
     },
 });
