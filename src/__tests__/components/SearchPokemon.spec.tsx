510a59dfe1e225eb0eeb6063c248c4d9c1c3f41d
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-07-02 01:50:20 +0000

    remove

--- src/__tests__/components/SearchPokemon.spec.tsx
+++ src/__tests__/components/SearchPokemon.spec.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from '@testing-library/user-event'
 import { SearchPokemon } from '../../components/SearchPokemon';
